093c772f0351576e40505ee6f608e9e5a9bab7fa
Can <hcdayan@hotmail.com>
2021-12-28 21:30:37 +0300

    Change db properties. Edit index. Change form.

--- ThiorkProject/ThiorkProject/settings.py
+++ ThiorkProject/ThiorkProject/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'thiorkdb',
         'USER': 'thiorkdb',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'thiorkdb',
         'HOST': 'db',
         'PORT': 5432
     }
